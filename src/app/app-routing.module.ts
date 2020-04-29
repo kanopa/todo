4a8e452253cece96f29fed7bc9264da5d4a9d4c0
kanopa <konoplyanka1999@gmail.com>
2020-04-29 04:27:10 +0300

    simple todo interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemComponent } from './item/item.component';
+import { ListComponent } from './list/list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'item', component: ItemComponent},
+  {path: 'list', component: ListComponent},
+  {path: '**', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
